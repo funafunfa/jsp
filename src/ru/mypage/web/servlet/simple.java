b4b9f127c738ab0d11e097b8af194c9038725628
funafunfa <golovolomka132@gmail.com>
2017-01-02 15:39:31 +0200

    Initial commit

---
+++ src/ru/mypage/web/servlet/simple.java
@@ -0,0 +1,17 @@
+package ru.mypage.web.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by dcamo on 1/2/2017.
+ */
+public class simple extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("LOLS");
+    }
+}
